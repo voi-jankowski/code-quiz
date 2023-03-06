8107cb95cde84ef840f307599bc5f35ac45d5cc8
voi <wojtek.wacek.dola@gmail.com>
2023-03-06 16:35:49 +1100

    Written pseudo-code for js file.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,33 @@
 // Variables related to elements in DOM
 
 
-// 
+// sound effects
+
+// When the player presses High Scores button - the high scores are displayed underneath.
+
+// When the player presses start button the quiz starts 
+// and the first question is displayed 
+// and the starter-info is hidden from view
+// and the timer starts running
+// disable high scores button
+
+// When the player presses one of the choices button: event key and compares with the answer.
+// if the choice is correct - next question
+// if the choice is incorrect - next question and timer -15sec
+
+// TIMER - is set to run for 75sec
+// When the questions are answered the timer stops and Game Over
+// When the timer finishes - Game Over
+// The time remaining in the timer becomes a high score
+
+// GAME OVER:
+// Display Game Over message 
+// and info about the scorewith text-box to record player's name
+
+// When the player presses submit button:
+// Set high scores and save them in the local storage with the player's name
+// Enable High Scores Button
+// Reset the quiz-box to starter-info.
+
+
+
